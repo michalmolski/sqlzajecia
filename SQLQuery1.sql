8588f4a915c36c4f410e8628259a61c1d471ea3c
michalmolski <michalmateuszmolski@gmail.com>
2019-03-29 22:14:09 +0100

    Proper inserting records to 'produkty' table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,44 +6,59 @@ CREATE DATABASE FirmaKomputerowa
 --DROP TABLE [FirmaKomputerowa].[dbo].[LAPTOP];
 --DROP TABLE [FirmaKomputerowa].[dbo].[DRUKARKI];
 
+CREATE TABLE [FirmaKomputerowa].[dbo].[Produkty]
+(
 
+	model nvarchar(30)  PRIMARY KEY NOT NULL,
+	producent nvarchar(30),
+	typ nvarchar(30),
+	--FOREIGN KEY (model) REFERENCES [FirmaKomputerowa].[dbo].[PC](model),
+	--FOREIGN KEY (model) REFERENCES [FirmaKomputerowa].[dbo].[LAPTOP](model),
+	--FOREIGN KEY (model) REFERENCES [FirmaKomputerowa].[dbo].[DRUKARKI](model) 
+
+);
 
 CREATE TABLE [FirmaKomputerowa].[dbo].[PC]
 (
-	model nvarchar(30) PRIMARY KEY NOT NULL,
+	model nvarchar(30) PRIMARY KEY,
 	procesor INT,
-	ram nvarchar(30),
-	hdd nvarchar(30),
+	ram INT,
+	hdd INT,
 	cd nvarchar(30),
 	ekran INT,
 	cena MONEY,
+	FOREIGN KEY (model) REFERENCES [FirmaKomputerowa].[dbo].[Produkty](model)
 
 );
 
 CREATE TABLE [FirmaKomputerowa].[dbo].[LAPTOP]
 (
-	model nvarchar(30) PRIMARY KEY NOT NULL,
+	model nvarchar(30) PRIMARY KEY ,
 	procesor INT,
 	ram INT,
 	hdd INT,
 	ekran INT,
 	cena MONEY,
+	FOREIGN KEY (model) REFERENCES [FirmaKomputerowa].[dbo].[Produkty](model)
 );
 
 CREATE TABLE [FirmaKomputerowa].[dbo].[DRUKARKI]
 (
-	model nvarchar(30) PRIMARY KEY NOT NULL,
+	model nvarchar(30) PRIMARY KEY ,
 	kolor BIT,
 	typ nvarchar(30),
 	cena MONEY,
+	FOREIGN KEY (model) REFERENCES [FirmaKomputerowa].[dbo].[Produkty](model)
 );
-CREATE TABLE [FirmaKomputerowa].[dbo].[Produkty]
-(
-	producent nvarchar(30),
-	model nvarchar(30)  PRIMARY KEY NOT NULL,
-	typ nvarchar(30),
-	FOREIGN KEY (model) REFERENCES [FirmaKomputerowa].[dbo].[PC](model),
-	FOREIGN KEY (model) REFERENCES [FirmaKomputerowa].[dbo].[LAPTOP](model),
-	FOREIGN KEY (model) REFERENCES [FirmaKomputerowa].[dbo].[DRUKARKI](model) 
+INSERT INTO [FirmaKomputerowa].[dbo].[Produkty](producent,model,typ) 
+VALUES		('DELL','2K8Q7','dom'),
+			('ACER','2K9D7','dom'),
+			('HP','3KS83','inny'),
+			('DELL','D93KS','dom'),
+			('ASUS','2KED7','BIURO'),
+			('HP','3KS22','inny'),
+			('LENOVO','E83KS','dom'),
+			('APPLE','2K9DS7','biuro'),
+			('HP','22G63','inny'),
+			('DELL','H28US','dom');
 
-);
