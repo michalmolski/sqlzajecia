3be469ae28894ee80848db1e7f66cae2065cd176
michalmolski <michalmateuszmolski@gmail.com>
2019-03-30 17:34:56 +0100

    INSERT SELECT ADDED- need to fill NULLS

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -51,14 +51,42 @@ CREATE TABLE [FirmaKomputerowa].[dbo].[DRUKARKI]
 	FOREIGN KEY (model) REFERENCES [FirmaKomputerowa].[dbo].[Produkty](model)
 );
 INSERT INTO [FirmaKomputerowa].[dbo].[Produkty](producent,model,typ) 
-VALUES		('DELL','2K8Q7','dom'),
-			('ACER','2K9D7','dom'),
-			('HP','3KS83','inny'),
-			('DELL','D93KS','dom'),
-			('ASUS','2KED7','BIURO'),
-			('HP','3KS22','inny'),
-			('LENOVO','E83KS','dom'),
-			('APPLE','2K9DS7','biuro'),
-			('HP','22G63','inny'),
-			('DELL','H28US','dom');
-
+VALUES		('DELL','2K8Q7','pc'),
+			('ACER','2K9D7','pc'),
+			('HP','3KS83','laptop'),
+			('DELL','D93KS','laptop'),
+			('ASUS','2KED7','drukarka'),
+			('HP','3KS22','drukarka'),
+			('LENOVO','E83KS','pc'),
+			('APPLE','2K9DS7','laptop'),
+			('HP','22G63','laptop'),
+			('DELL','H28US','drukarka'),
+			('DELL','3K8Q7','pc'),
+			('ACER','2J9D7','drukarka'),
+			('HP','3KS43','pc'),
+			('DELL','WE3KS','laptop'),
+			('ASUS','4RED7','laptop'),
+			('HP','3KL02','pc'),
+			('LENOVO','E80KS','laptop'),
+			('APPLE','2K9KO','pc'),
+			('HP','22RR8','drukarka'),
+			('DELL','H2P4S','pc'),
+			('DELL','2E4Q7','pc'),
+			('ACER','2K9D0','laptop'),
+			('HP','3KS80','drukarka'),
+			('DELL','D93K0','laptop'),
+			('ASUS','2KEY7','drukarka'),
+			('HP','3KS2Y','drukarka'),
+			('LENOVO','Q83KS','drukarka'),
+			('APPLE','2K9M7','pc'),
+			('HP','X2G63','laptop'),
+			('DELL','HX8US','drukarka');
+INSERT INTO [FirmaKomputerowa].[dbo].[PC](model)
+SELECT model FROM [FirmaKomputerowa].[dbo].[Produkty]
+WHERE typ='pc'
+INSERT INTO [FirmaKomputerowa].[dbo].[LAPTOP](model)
+SELECT model FROM [FirmaKomputerowa].[dbo].[Produkty]
+WHERE typ='laptop'
+INSERT INTO [FirmaKomputerowa].[dbo].[DRUKARKI](model)
+SELECT model FROM [FirmaKomputerowa].[dbo].[Produkty]
+WHERE typ='drukarka'
