f6e90d6b56529420afed02fcae3ca3c486eaa307
michalmolski <michalmateuszmolski@gmail.com>
2019-03-31 17:46:51 +0200

    pROPER QUERY PREPARED FOR NEXT EXERCISES

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -81,12 +81,40 @@ VALUES		('DELL','2K8Q7','pc'),
 			('APPLE','2K9M7','pc'),
 			('HP','X2G63','laptop'),
 			('DELL','HX8US','drukarka');
-INSERT INTO [FirmaKomputerowa].[dbo].[PC](model)
-SELECT model FROM [FirmaKomputerowa].[dbo].[Produkty]
-WHERE typ='pc'
-INSERT INTO [FirmaKomputerowa].[dbo].[LAPTOP](model)
-SELECT model FROM [FirmaKomputerowa].[dbo].[Produkty]
-WHERE typ='laptop'
-INSERT INTO [FirmaKomputerowa].[dbo].[DRUKARKI](model)
-SELECT model FROM [FirmaKomputerowa].[dbo].[Produkty]
-WHERE typ='drukarka'
+
+INSERT INTO [FirmaKomputerowa].[dbo].[PC](procesor,ram,hdd,cd,ekran,cena,model)
+VALUES 
+		(3400,8,512,'tak',12,1200,'2E4Q7'),
+		(2300,16,1024,'tak',15,1500,'2K8Q7'),
+		(3400,8,1024,'tak',17,1900,'2K9D7'),
+		(3300,8,1024,'tak',15,1800,'2K9KO'),
+		(3100,16,1024,'tak',19,2200,'2K9M7'),
+		(2900,16,2048,'nie',19,2100,'3K8Q7'),
+		(3000,8,2048,'nie',12,1700,'3KL02'),
+		(2900,8,2048,'nie',12,1400,'3KS43'),
+		(2700,32,2048,'nie',21,2900,'E83KS'),
+		(3000,32,2048,'nie',21,3000,'H2P4S');
+INSERT INTO [FirmaKomputerowa].[dbo].[LAPTOP](procesor,ram,hdd,ekran,cena,model)
+VALUES 	
+		(3400,8,512,12,1200.00,'22G63'),
+		(2800,8,1024,15,1500.00,'2K9D0'),
+		(3400,16,1024,17,2100.00,'2K9DS7'),
+		(3300,8,1024,15,1800.00,'3KS83'),
+		(3100,16,1024,17,2200.00,'4RED7'),
+		(2400,16,2048,15,2100.00,'D93K0'),
+		(3000,8,2048,17,1700.00,'D93KS'),
+		(2800,4,1024,12,1400.00,'E80KS'),
+		(2700,32,2048,15,2900.00,'WE3KS'),
+		(3900,32,2048,15,3600.00,'X2G63');
+INSERT INTO [FirmaKomputerowa].[dbo].[DRUKARKI](kolor,typ,cena,model)
+VALUES	
+(1, 'biuro' ,600.0, '22RR8'),
+(1, 'dom' ,500.0, '2J9D7'),
+(1, 'dom' ,520.0, '2KED7'),
+(0, 'biuro' ,800.0, '2KEY7'),
+(0, 'biuro' ,500.0, '3KS22'),
+(1, 'dom' ,456.0, '3KS2Y'),
+(0, 'dom' ,530.0, '3KS80'),
+(1, 'biuro' ,180.0, 'H28US'),
+(1, 'biuro' ,600.0, 'HX8US'),
+(0, 'dom' ,350.0, 'Q83KS');
